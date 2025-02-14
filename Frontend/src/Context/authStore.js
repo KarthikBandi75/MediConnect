47247aa099bedc534643a92740f6daaf05839b6d
BANDI KARTHIK <bandikarthik75@gmail.com>
2025-02-14 18:49:22 +0530

    Update authStore.js

--- Frontend/src/Context/authStore.js
+++ Frontend/src/Context/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const API_URL= import.meta.env.VITE_BACKEND_URL;
+const API_URL= "https://mediconnect-backend-production.up.railway.app";
 
 
 axios.defaults.withCredentials = true;
