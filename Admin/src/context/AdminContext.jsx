0fca845637afe39a8f2c4a7b43058912c5ce00b4
BANDI KARTHIK <bandikarthik75@gmail.com>
2025-02-14 18:47:37 +0530

    Update AdminContext.jsx

--- Admin/src/context/AdminContext.jsx
+++ Admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
   const [doctors, setDoctors] = useState([]);
   const [appointments, setAppointments] = useState([]);
   const [dashData, setDashData] = useState(false);
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://mediconnect-backend-production.up.railway.app";
 
   const getAllDoctors = async () => {
     try {
